51943e8378cb8d5581e2a7c66934e0eda56975d2
JotaK1 <jp.ausensi@gmail.com>
2021-03-21 21:06:00 -0300

    terminado v0

---
+++ src/context/CategoriasContext.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import React, { createContext, useEffect, useState } from 'react';
+
+
+// Crear el context
+export const CategoriasContext = createContext();
+
+// Provider es donde se encuentrar las funciones y state
+const CategoriasProvider = (props) => {
+
+    // crear el state del Context
+    const [ categorias, guardarCategorias] = useState([]);
+
+    // ejecutar el llamado a la api
+    useEffect(()=>{
+        const obtenerCategorias = async () => {
+            const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+            const categoriasAx = await axios.get(url);
+            guardarCategorias(categoriasAx.data.drinks);
+        }    
+        obtenerCategorias();
+    },[]);
+    return (
+        <CategoriasContext.Provider
+            value={{
+                categorias
+            }}
+        >
+            {props.children}
+        </CategoriasContext.Provider>
+    )
+}
+export default CategoriasProvider;
